d067f06ea849992475f3c1547258517412517992
Bitseat <yheysbitseat96@gmail.com>
2025-11-05 22:07:39 -0800

    tasks can be broken down into subtasks

--- src/Task.java
+++ src/Task.java
@@ -1,26 +1,43 @@
-import java.time.Duration;
+
 import java.time.LocalDateTime;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
 
 public class Task {
     private  String taskContent;
-
     private   LocalDateTime dueTime;
-    public  enum State{DONE,UNDONE};
+
+    public  enum State{PENDING, inPROGRESS,COMPLETED,OVERDUE,ARCHIVED}
     private State state;
+    private  List<Task> subTasks;
 
     public Task(String taskContent,LocalDateTime dueTime){
         this.dueTime =dueTime;
         this.taskContent=taskContent;
-        state=State.UNDONE;
-
-
-
+        state=State.PENDING;
 
     }
+    public Iterator<Task> getSubTasks(){
+        if(subTasks==null||subTasks.isEmpty()){
+            return null;
+        }
+        return  subTasks.iterator();
+    }
 
+    public void addSubTask(Task task){
+        if(subTasks==null){
+             subTasks=new ArrayList<>();
+        }
+        subTasks.add(task);
+        subTasks.sort(Comparator.comparing(Task::getDueTime));
+    }
+
+    public void  removeSubTask(Task subTask){
+            subTasks.remove(subTask);
+    }
 
 
     public void setState(State state) {
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -13,21 +13,35 @@ public class TaskManager  {
 
 
 
+  public  static  Iterator<Task> getTask(){
+      return toDoList.iterator();
 
+  }
     public static void addTask(String taskContent, LocalDateTime dueTime){
         Task task=new Task(taskContent,dueTime);
         toDoList.add(task);
         toDoList.sort(Comparator.comparing(Task::getDueTime));
         setTimer(task);
-
-
     }
+
+    public static  void addSubTask(Task task,String taskContent,LocalDateTime dueTime){
+          Task subTask=new Task(taskContent,dueTime);
+          task.addSubTask(subTask);
+           setTimer(subTask);
+    }
+
+
+
+
+
     public static  void setTimer(Task task){
         try {
             long delay= Duration.between(LocalDateTime.now(),task.getDueTime().minusMinutes(30)).toMillis();  //30 min before due time
             scheduler.schedule(()->{              //background thread that waits delay amount of time to execute command
-                if(task.getState()== Task.State.UNDONE)
-                    System.out.printf("task: %s  due time is within 30 minutes",task.getTaskContent());
+                if(task.getState()== Task.State.PENDING) {
+                    System.out.printf("task: %s  due time is within 30 minutes", task.getTaskContent());
+                   task.setState(Task.State.OVERDUE);
+                }
             },delay, TimeUnit.SECONDS);
 
             scheduler.schedule(TaskManager::closeTimer,delay+60,TimeUnit.SECONDS);
@@ -42,23 +56,29 @@ public class TaskManager  {
         scheduler.shutdown();
     }
 
-    public static void checkOffTask(int index){
-        toDoList.get(index).setState(Task.State.DONE);       //accessing is o(1)
+    public static void checkOffTask(Task task){
+          task.setState(Task.State.COMPLETED);       //accessing is o(1)
     }
 
-    public static void removeTask(int index){
-         toDoList.remove(index);         //o(n) in the worst case
+    public static void removeTask(Task task){              //
+         task.setState(Task.State.ARCHIVED);
+         toDoList.remove(task);
+                                                                //o(n) in the worst case
     }
-    public static  void changeDueTimeOfTask(int index,LocalDateTime newDueTime){
-          toDoList.get(index).setDueTime(newDueTime);
+    public  static  void removeSubTask(Task task,Task subTask){
+           task.removeSubTask(subTask);
+    }
+
+    public static  void postPoneTask(Task task,LocalDateTime newDueTime){
+           task.setDueTime(newDueTime);
           toDoList.sort(Comparator.comparing(Task::getDueTime));
     }
 
 
 
-    public static  void viewTasks(){       //show only undone tasks
+    public static  void viewTasks(){       //show only undone&& half completed tasks
         System.out.printf("%-3s%-15s%25s%n","","Task","Due Time");
-        List<Task> undoneTasks=toDoList.stream().filter(task->task.getState()== Task.State.UNDONE).toList();
+        List<Task> undoneTasks=toDoList.stream().filter(task->task.getState()== Task.State.PENDING||task.getState()== Task.State.inPROGRESS).toList();
 
         for(int i=0;i< undoneTasks.size();i++){
            System.out.printf("%-3d%-20s%25s%n",(i+1),undoneTasks.get(i).getTaskContent(),
@@ -66,6 +86,16 @@ public class TaskManager  {
         }
 
     }
+
+    public static void viewSubTasks(Task task){
+       while (task.getSubTasks().hasNext()){
+
+           System.out.println(task.getSubTasks().next().getTaskContent());
+       }
+
+    }
+
+
 
 
 
